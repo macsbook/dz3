6be11a077e4fee6f9019edd5f60b3cd9bf3b5bd7
macsbook <ilgizsaparbekov@gmail.com>
2022-10-12 16:31:26 +0600

    a

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const sport = ["football", "basketball","volleyball","tennis","bouncers"]
 function sportPick (word, array){
     for (let i = 0; i< array.length; i++){
-        if(word ==  array[i]){
+        if(word ===  array[i]){
             return console.log( array[i],'найден')
         }
     }
 }
-sportPick('football', sport)
+sportPick('football', s
